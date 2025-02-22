354a5c900310b8587088cc8c1e03dbc7a97e0f22
Isi <isidro.santamaria@googlemail.com>
2025-02-22 21:08:25 +0000

    First commit of the MultiSnake

---
+++ routes/users.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql2');
+
+
+// Create a MySQL connection
+const db = mysql.createConnection({
+  host: 'localhost', // Your MySQL server
+  user: 'root', // Replace with your MySQL username
+  password: 'M0nkey12', // Replace with your MySQL password
+  database: 'snake' // Your database name
+});
+
+// Connect to MySQL
+db.connect(err => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as id', db.threadId);
+});
+
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+
+  // Execute the query
+  db.query('SELECT id, username FROM game_user', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      return res.status(500).json({ error: 'Database query failed' });
+    }
+    res.json(results); // Send the users as JSON response
+  });
+
+
+
+
+  // res.send('respond with a resource');
+});
+
+module.exports = router;
